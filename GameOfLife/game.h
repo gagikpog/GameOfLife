c37c9f017db7279307f5be4222cd4a5390fc161d
Gagik <gagikpog@gmail.com>
2019-04-04 12:22:21 +0500

    MinGW_Support

--- GameOfLife/game.h
+++ GameOfLife/game.h
@@ -1,6 +1,9 @@
-#include  <GL/freeglut.h>
+#include <GL/freeglut.h>
 #include <ctime>
 #include <string>
+#include <stdlib.h>
+#include <sstream>
+#include <iostream>
 
 #ifndef GAME_H
 #define GAME_H
@@ -14,6 +17,16 @@ bool **CreateMatrix(int width, int height);
 void RandomFilling( bool ** source, int width, int height);
 
 void drawString(float x, float y, std::string str);
+#ifdef __MINGW32__
+    namespace std{
+        template<typename T>
+        std::string to_string(T t){
+            std::stringstream ss;
+            ss<<t;
+            return ss.str();
+        }
+    }
+#endif
 
 #endif // GAME_H
 
